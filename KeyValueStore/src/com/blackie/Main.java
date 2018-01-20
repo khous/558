b3c84c9c529ed0a97df7934d08ce2487d627e5b4
Kyle <kyle@bill4time.com>
2018-01-20 14:55:37 -0800

    adding source

---
+++ KeyValueStore/src/com/blackie/Main.java
@@ -0,0 +1,83 @@
+package com.blackie;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    /**
+     * The port on which to listen
+     */
+    private static final int PORT = 1337;
+    /**
+     * The backing datastructure for the key value store
+     */
+    private static final Map<String, String> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        int port = PORT;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        ServerSocket listener = new ServerSocket(port);
+
+        try {
+            while (true) {
+                new Server(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    /**
+     * The threaded Server
+     */
+    private static class Server extends Thread {
+        private Socket socket;
+
+        Server (Socket socky) {
+            socket = socky;
+        }
+
+        @Override
+        public void run() {
+            try (
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            ) {
+                out.println();
+                String input = in.readLine();
+                System.out.println("Receiving\t\t" + input + "\t\tat " + (new Date()).toString());
+
+                int idxOfColon = input.indexOf(":");
+                String output = "key/value saved";
+                if (idxOfColon > 0) {
+                    String k = input.substring(0, idxOfColon);
+                    String v = input.substring(idxOfColon + 1);
+                    synchronized (map) {
+                        map.put(k, v);
+                    }
+                } else {
+                    synchronized (map) {
+                        output = map.getOrDefault(input, "null");
+                    }
+                }
+                System.out.println("Sending\t\t" + output + "\t\tat " + (new Date()).toString());
+                out.println(output);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ KeyValueStoreClient/src/com/blackie/Main.java
@@ -0,0 +1,52 @@
+package com.blackie;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String serverAddress = args[0];
+        String port = args[1];
+        String message = args.length >= 3 ? args[2] : "";
+
+        //Test code for stress test
+        /*for (int i = 0; i < 1000; i++) {
+            sendMessage(serverAddress, port, i + ":bitches");
+        }
+        for (int i = 0; i < 1000; i++) {
+            sendMessage(serverAddress, port, i + "");
+        }*/
+
+        sendMessage(serverAddress, port, message);
+    }
+
+    /**
+     * Send a request to store or retrieve a key
+     * @param serverAddress The text address of the server to send the request to. This can be an IP or a domain name
+     * @param port The port to send the request to
+     * @param message The message to send in the format of key:value to save a value or key to retrieve a value
+     * @throws IOException Ain't got time for try catch
+     */
+    public static void sendMessage (String serverAddress, String port, String message) throws IOException {
+        Socket socky = new Socket(serverAddress, Integer.parseInt(port));
+
+        try (
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socky.getInputStream()));
+                PrintWriter out = new PrintWriter(socky.getOutputStream(), true);
+        ) {
+            String line;
+            StringBuilder sb = new StringBuilder();
+
+            line = in.readLine();
+//                if (line == null || line.length() == 0) break;
+            System.out.println("Requesting\t\t" + message + "\t\tat " + (new Date()).toString());
+            out.println(message);
+            String response = in.readLine();
+            System.out.println("Received\t\t" + message + "\t\tat " + (new Date()).toString());
+        }
+
+    }
+}
